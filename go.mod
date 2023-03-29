777fabda5a2ba5d0faa144057ea38c9d63dd50c7
Hirochon <heacet43@gmail.com>
2023-03-29 22:11:46 +0900

    add: gpt4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Hirochon/try-gpt-4
 
 go 1.20
+
+require github.com/sashabaranov/go-openai v1.5.7
--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	client := openai.NewClient(os.Getenv("AUTH_TOKEN"))
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: `Hello`,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+	fmt.Println(resp.Choices[0].Message.Content)
 }
